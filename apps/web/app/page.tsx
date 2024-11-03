ba0a01ce3a79ed247f52dea0ad341a6fb9c9b246
Mike Wolf <mw@mike-wolf.com>
2024-11-03 16:34:28 -0500

    Changes to plan

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>
+            WEBBBBBBBBBB You can GET started by RTYING <code>app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
